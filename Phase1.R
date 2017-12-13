dc2c1fa0936e6f8e37fb9a3d4153b92dfdaeb6c0
Javad <abdijavad110@gmail.com>
2017-12-13 13:33:37 +0330

    Add Regnrator

--- Phase1.R
+++ Phase1.R
@@ -1,3 +1,29 @@
+#1
+# we are using Linear congruential generator its better not to change the parameters
+regnrator <- function(m = 2 ^ 48, a = 25214903917, c = 11, floor = 0,  roof = 1, n = 1, Integer = FALSE){
+  
+  distance <- roof - floor
+  
+  #seed(using system time for most randomness):
+  seed <- as.numeric(Sys.time()) * 1000
+  
+  #output numbers:
+  numbers <- vector(length = n)
+  
+  seed <- (a * seed + c) %% m
+  for(i in 1:n){
+    seed <- (a * seed + c) %% m
+    numbers[i] <- (seed / m) * distance + floor
+  }
+  
+  if(Integer)
+    for(i in 1:n)
+      numbers[i] <- as.integer(numbers[i])
+  
+  return(numbers)
+  
+}
+
 #3
 cugen <- function(){
   return(dugen(1,0))
